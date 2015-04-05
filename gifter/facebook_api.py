b7929bdc5aa1b0464c082dfd86af68fab2ac1fe6
rivinek <rivinek@gmail.com>
2015-04-05 18:01:00 +0200

    Used requests in fb api

--- gifter/facebook_api.py
+++ gifter/facebook_api.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python
 import facebook
-import urllib
 import urlparse
-import subprocess
+import requests
 
 from errors import (
     FacebookApiError,
@@ -11,29 +10,19 @@ from errors import (
 from gifter.config import (
     FACEBOOK_APP_ID,
     FACEBOOK_APP_SECRET,
-    FACEBOOK_PROFILE_ID,
-    FB_ACCESS_TOKEN_URL,
+    # FACEBOOK_PROFILE_ID,
 )
 
 
 def get_fb_access_token():
-    # Trying to get an access token. Very awkward.
-    oauth_args = dict(
-        client_id=FACEBOOK_APP_ID,
-        client_secret=FACEBOOK_APP_SECRET,
-        grant_type='client_credentials'
-    )
-    oauth_url = FB_ACCESS_TOKEN_URL.format(urllib.urlencode(oauth_args))
-    oauth_curl_cmd = ['curl', oauth_url]
-    oauth_response = subprocess.Popen(
-        oauth_curl_cmd,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE
-    ).communicate()[0]
-
+    params = {'grant_type': 'client_credentials',
+              'client_id': FACEBOOK_APP_ID,
+              'client_secret': FACEBOOK_APP_SECRET}
+    oauth_response = requests.post(
+        'https://graph.facebook.com/oauth/access_token?', params=params)
     try:
         return urlparse.parse_qs(
-            str(oauth_response)
+            str(oauth_response.text)
         )['access_token'][0]
     except KeyError:
         raise NoAccesTokenError('Unable to grab an access token!')
@@ -43,14 +32,8 @@ if __name__ == '__main__':
     """Only for testing"""
     oauth_access_token = get_fb_access_token()
     facebook_graph = facebook.GraphAPI(oauth_access_token)
-
     try:
-        # Try to post something on the wall.
-        fb_response = facebook_graph.put_wall_post(
-            'Test wall', profile_id=FACEBOOK_PROFILE_ID
-        )
-        print fb_response
+        print facebook_graph.get_object("me")
     except facebook.GraphAPIError as e:
-        raise FacebookApiError(
-            'Type error: {}, message: {}'.format(e.type, e.message)
-        )
+        raise FacebookApiError('Type error: {}, message: {}'.format(e.type,
+                                                                    e.message))
