0f3b7b32e6f044794586c4f656e05894e5bb0b15
rivinek <rivinek@gmail.com>
2015-04-22 11:04:45 +0200

    Ahhh dat Marta

--- gifter/recipient.py
+++ gifter/recipient.py
@@ -14,14 +14,14 @@ auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
 # Creation of the actual interface, using authentication
 api = tweepy.API(auth)
 
-#Searching for the recipent
+# Searching for the recipent
 search_user = raw_input('Enter a name of  recipient: ')
 list_of_finded_users = api.search_users(search_user)
 
 if api.search_users(search_user) == []:
     print("We can not find such person on Twitter.")
 else:
-    #Printing all fitting users
+    # Printing all fitting users
     for user in list_of_finded_users:
         if user.location:
             print("Location of user: " + user.screen_name + " is " +
@@ -32,7 +32,7 @@ else:
     print(api.get_user(recipient.screen_name))
     print(api.get_user(recipient.screen_name).profile_image_url)
 
-    #Saving status of recipent in a text file
+    # Saving status of recipent in a text file
     with open("document_with_status.txt", 'w') as document_with_status:
         for status in tweepy.Cursor(api.user_timeline,
                                     id=recipient.id,
@@ -40,6 +40,6 @@ else:
             print(status)
             print >> document_with_status, status
 
-    #Print all people that recipient is following
+    # Print all people that recipient is following
         for friend in recipient.friends():
             print(friend.screen_name)
