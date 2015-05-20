bdb625c8a2a5b07f8ec227d96d38649effa70ac7
bckatarzyna <bckatarzyna@gmail.com>
2015-05-20 21:45:18 +0100

    new result view + loader modal while loading tweets

--- gifter/static/public/app/newPresent/newPresent.js
+++ gifter/static/public/app/newPresent/newPresent.js
@@ -1,16 +1,24 @@
 angular.module('gifter.newPresent', [])
 
-    .controller('NewPresentCtrl', ['$state', 'stateService', '$http', 'storageService',
-    	function ($state, stateService, $http, storageService) {
+    .controller('NewPresentCtrl', ['$state', '$modal', 'stateService', '$http', 'storageService',
+    	function ($state, $modal, stateService, $http, storageService) {
 
     		stateService.setState($state.current.name);
 
     		this.twitterName = '';
     		this.from = '';
     		this.to = '';
+            this.modalInstance;
+
+            this.openModal = function (size) {
+                this.modalInstance = $modal.open({
+                  templateUrl: 'app/common/viewComponents/modal/modal.html'
+                });
+            };
 
     		this.find = function () {
                 that = this;
+                that.openModal();
     			var url = 'http://127.0.0.1:5000/api/items/' + this.twitterName + '/';
     			$http.post(url, {
     				'min_price': this.from || 0,
@@ -20,6 +28,7 @@ angular.module('gifter.newPresent', [])
                     storageService.twitterName = that.twitterName;
         			storageService.savePresents(res.gifts);
                     storageService.hashtags = res.hashtags;
+                    that.modalInstance.close();
         			$state.go('main.result');
         		});
     		};
