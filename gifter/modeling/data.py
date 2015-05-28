da7b91b9dc689877f62fa38a95141ac5dec80834
Mateusz Flieger <mateuszflieger@gmail.com>
2015-05-28 10:59:09 +0200

    Pandas bug workaround

--- gifter/modeling/data.py
+++ gifter/modeling/data.py
@@ -13,11 +13,12 @@ def lemmatized_frame(filename="../data/data.json", last_rows=300, with_tags=True
     df = pd.read_json(filename)
     df.sort('created_at', ascending=False, inplace=True)
     df = df[df.lang == 'en']  # only en
-    df = df[:last_rows]
 
     # bug in pandas it's a hack
-    df['text_len'] = df.text.map(len)
+    df['text_len'] = df.text.map(lambda text: len(text.split()))
     df.sort('text_len', ascending=False, inplace=True)
+    df.reset_index(inplace=True)
+    df = df[:last_rows]
 
     df['lemmas'] = df[['text', 'entities']].apply(
         lambda row: lemmatize_by_row(row, with_tags),
