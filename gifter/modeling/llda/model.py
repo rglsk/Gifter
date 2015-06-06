af33a44c00ab48afb06b39f4e2696860b18f2eed
Piotr Rogulski <piotr.rogulski@unit9.com>
2015-06-06 17:45:01 +0200

    OMG SMTH DZIALA

--- gifter/modeling/llda/model.py
+++ gifter/modeling/llda/model.py
@@ -1,5 +1,7 @@
+import sys
 from itertools import izip
 
+import numpy as np
 import pandas as pd
 
 from gifter.modeling.models import BaseModel
@@ -17,16 +19,39 @@ class LldaModel(BaseModel):
 
         self.llda = LLDA(TOPIC_NUMBER, alpha, beta)
 
+    def _get_storage(self):
+        return
+
     def train(self):
         training_set = {category: [] for category in self.output_train}
-        for train_path, category in izip(self.inputs_train, self.output_train):
+        zipped_data = izip(self.inputs_train.preprocessed_filename,
+                           self.output_train)
+        for train_path, category in zipped_data:
             tweets_df = pd.read_json(train_path)
             training_set[category] += (tweets_df.lemmas.sum())
 
-        labelset = set(training_set.keys())
+        corpus = training_set.values()
+        labels = [[key] for key in training_set.keys()]
+        labelset = list(set(reduce(list.__add__, labels)))
+        print 'poszlo'
         self.llda.set_corpus(labelset,
-                             training_set.values(),
-                             training_set.keys())
+                             corpus,
+                             labels)
+        print "M={}, V={}, L={}, K={}".format(len(corpus),
+                                              len(self.llda.vocas),
+                                              len(labelset),
+                                              TOPIC_NUMBER)
+
+        for i in range(TOPIC_NUMBER):
+            sys.stderr.write("-- %d : %.4f\n" % (i, self.llda.perplexity()))
+            self.llda.inference()
+        print "perplexity : %.4f" % self.llda.perplexity()
+
+        phi = self.llda.phi()
+        for k, label in enumerate(labelset):
+            print "\n-- label %d : %s" % (k, label)
+            for w in np.argsort(-phi[k])[:20]:
+                print "%s: %.4f" % (self.llda.vocas[w], phi[k, w])
 
     def predict_one(self, one):
         """
@@ -39,3 +64,10 @@ class LldaModel(BaseModel):
         :param inputs: list of preprocessed twitter DataFrames
         """
         return
+
+
+
+if __name__ == '__main__':
+    llda = LldaModel(alpha=0.001, beta=0.001)
+    llda.train()
+    import ipdb; ipdb.set_trace()
