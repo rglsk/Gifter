601ce35fa58e128618718faa96c8be8af4bd6df8
martap6 <marta.puc3@gmail.com>
2015-06-08 04:22:50 +0200

    lda

--- gifter/modeling/lda/lda.py
+++ gifter/modeling/lda/lda.py
@@ -1,94 +1,96 @@
-# coding: utf-8
-
+#!/usr/bin/env python
 import itertools
 import gensim
-from gifter.modeling.data import lemmatized_frame
+from gensim import corpora, models
 from gifter.modeling.tokenizer import lemmatize
-from gensim import corpora
+import pandas as pd
 
 
-def LDA(texts=["default"], num=2, passes=100):
-    if (texts == ["default"]):
-        df = lemmatized_frame("../data.json", with_tags=False)
-        texts = [df['lemmas'].irow(i) for i in range(df.shape[0])]
+def lda(
+    list_of_json=["../data/data.json", "../data/data2.json"],
+    documents_list=[],
+    num=24,
+    passes=20,
+    save_model_as='../lda/lda.model',
+    save_dic_as='../lda/dictionary.dic'
+):
+    texts2 = []
+    table = []
+    if not documents_list:
+        for document in list_of_json:
+            print str(document)
+            df = pd.io.json.read_json(document)
+            for i in range(df.shape[0]):
+                table = table + df['lemmas'].irow(i)
+            texts2 = texts2 + [table]
     else:
         texts2 = []
-        for d in texts:
-            texts2 = texts2 + [lemmatize(d, with_tags=False)]
-            texts = texts2
-    dictionary = corpora.Dictionary(texts)
-    corpus = [dictionary.doc2bow(text) for text in texts]
-    corpora.MmCorpus.serialize('./corpus.mm', corpus)
-    mm_corpus = corpora.MmCorpus('./corpus.mm')
+        for d in documents_list:
+            texts2 = texts2 + [lemmatize(text=d, with_tags=False)]
+    dictionary = corpora.Dictionary(texts2)
+    dictionary.save(save_dic_as)
+    corpus = [dictionary.doc2bow(text) for text in texts2]
+    corpora.MmCorpus.serialize('../lda/corpus.mm', corpus)
+    mm_corpus = corpora.MmCorpus('../lda/corpus.mm')
+    print "mm_corpus: " + str(mm_corpus)
     id2word = {}
     for word in dictionary.token2id:
         id2word[dictionary.token2id[word]] = word
+    print "Made id2word"
     lda = gensim.models.ldamulticore.LdaMulticore(
         corpus=mm_corpus,
         num_topics=num,
         id2word=id2word,
-        eval_every=1,
         passes=passes,
         workers=1
     )
+    lda.save(save_model_as)
     for i in range(0, lda.num_topics):
-        print "Topic number " + str(
-            i
-        ) + " consists of words : " + lda.print_topic(i)
+        print(
+            "Topic number " +
+            str(i) + " consists of words : " + lda.print_topic(i)
+        )
     return lda, dictionary
 
-l, dic = LDA()
 
-documents = [
-    "Apple is releasing a new product",
-    "I love apple",
-    "Amazon sells many things",
-    "Microsoft announces Nokia acquisition",
-    "Human machine interface for lab abc computer applications",
-    "A survey of user opinion of computer system response time",
-    "The EPS user interface management system",
-    "System and human system engineering testing of EPS",
-    "Relation of user perceived response time to error measurement",
-    "The generation of random binary unordered trees",
-    "The intersection graph of paths in trees",
-    "Graph minors IV Widths of trees and well quasi ordering",
-    "Graph minors A survey"
-]
-
-D = [
-    "I like to eat broccoli and bananas.",
-    "I ate a banana and spinach smoothie for breakfast.",
-    "Chinchillas and kittens are cute.",
-    "My sister adopted a kitten yesterday.",
-    "Look at this cute hamster munching on a piece of broccoli."
-]
-
-l, dic = LDA(texts=D, num=2)
-
-l, dic = LDA(texts=documents, num=2)
+def count_words(lemmatized_words):
+    return {word: lemmatized_words.count(word) for word in lemmatized_words}
 
 
-def find_topic(new_doc="User", dictionary=dic, lda=l):
-    if (new_doc == "User"):
-        df = lemmatized_frame("../data.json", with_tags=False)
-        new_doc = list(
-            itertools.chain(
-                *[df['lemmas'].irow(i) for i in range(df.shape[0])]
-            )
+def learned_categories(train=[]):
+    res = {i: [] for i in xrange(0, 24)}
+    result = []
+    for document in train:
+        t = find_topic(frame=pd.io.json.read_json(document))
+        if (t[0][0]):
+            res[t[0][0]].append(document.split('/')[-2])
+    for i in res.keys():
+        if len(res[i]) > 0:
+            a = sorted(
+                count_words(res[i]).values(), reverse=True
+                )[0]
+            for key, value in count_words(res[i]).iteritems():
+                if (value == a):
+                    result.append(key)
+                    a = a+1
+        else:
+            result.append(u'science')
+    return result
+
+
+def find_topic(
+    frame=pd.io.json.read_json("../data/data.json"),
+    dictionary='../lda/dictionary2.dic',
+    lda='../lda/lda2.model'
+):
+    df = frame
+    new_doc = list(
+        itertools.chain(
+            *[df['lemmas'].irow(i) for i in range(df.shape[0])]
         )
-    else:
-        new_doc = new_doc.lower().split()
-    new_vec = dictionary.doc2bow(new_doc)
-    vec_lda = sorted(lda[new_vec], key=lambda vec: vec[1], reverse=True)
-    print str(vec_lda)
-    print "Top topic is topic number " + str(
-        vec_lda[0][0]
-    ) + " consists of words : " + lda.print_topic(vec_lda[0][0])
+    )
+    l2 = models.LdaModel.load(lda)
+    dic2 = corpora.Dictionary.load(dictionary)
+    new_vec = dic2.doc2bow(new_doc)
+    vec_lda = sorted(l2[new_vec], key=lambda vec: vec[1], reverse=True)
     return vec_lda
-
-
-find_topic()
-
-find_topic(
-    "I love watching films with my family and friends. I will write review of the film we watched yesterday"
-)
