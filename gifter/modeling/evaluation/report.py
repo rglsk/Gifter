5572299da860d02b98416cf90d740f3e32b1ff88
Piotr Rogulski <piotr.rogulski@unit9.com>
2015-06-08 11:20:49 +0200

    report llda

--- gifter/modeling/evaluation/report.py
+++ gifter/modeling/evaluation/report.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from sklearn.metrics import classification_report
 
+from gifter.utils import get_category_from_filepath
+from gifter.modeling.llda.model import LldaModel
 from gifter.modeling.evaluation.separate import separeted_data
 from gifter.modeling.word2vec.model import Word2VecModel
 
@@ -27,3 +29,19 @@ def create_report():
             output_test,
             predicted,
         )
+
+
+def llda_report():
+    inputs_test = pd.read_json('trol.json')
+    llda = LldaModel()
+    preprocessed = inputs_test.preprocessed_filename.tolist()
+    output_test = map(get_category_from_filepath, preprocessed)
+
+    predicted = llda.predict_many(
+        [pd.read_json(filename) for filename in preprocessed]
+    )
+    print llda.name
+    print classification_report(
+        output_test,
+        predicted,
+    )
