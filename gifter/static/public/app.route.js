38c936f04ecacaaa19b6eb816c9b66efe1a37c9c
bckatarzyna <bckatarzyna@gmail.com>
2015-06-09 18:04:17 +0200

    newPresent available under main url

--- gifter/static/public/app.route.js
+++ gifter/static/public/app.route.js
@@ -6,6 +6,10 @@ angular.module('gifter.route', ['gifter.viewComponents.route'])
             .state('main', {
                 url: '',
                 views: {
+                	'content@': {
+                        templateUrl: 'app/newPresent/newPresent.html',
+                        controller: 'NewPresentCtrl as NewPresentCtrl'
+                    },
                     'panel@': ViewComponents.panel
                 }
             });
