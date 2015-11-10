1bd14540ff18b1368afa7ac2971305168d14d9b0
Mateusz Flieger <mateuszflieger@gmail.com>
2015-11-10 15:53:43 +0100

    Imports BugFix

--- src/manage_gifter.py
+++ src/manage_gifter.py
@@ -8,7 +8,7 @@ from gifter.api.gifter_api import gifter_api
 
 app = Flask(__name__)
 app.config.from_object(os.environ.get('GIFTER_CONFIG_MODULE',
-                                      'gifter.config'))
+                                      'core.config'))
 app.register_blueprint(gifter_api)
 
 
