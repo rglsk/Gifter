d04870448520bb507a6621dad55f69711f9d4a60
Mateusz Flieger <mateuszflieger@gmail.com>
2015-11-14 18:56:58 +0100

    Filter out nans from mapping

--- src/ml/gifts/process.py
+++ src/ml/gifts/process.py
@@ -7,7 +7,10 @@ from core.config import DATA_DIRECTORY
 
 def get_ebay_categories_mapping():
     df = pd.read_json(path.join(DATA_DIRECTORY, 'category_mapping.json'))
+
+    ebay_categories = df[['CategoryName', 'CategoryName2']].values.tolist()
     return dict(
         zip(df['OurCategoryName'],
-            df[['CategoryName', 'CategoryName2']].values.tolist())
+            map(lambda cats:
+                [cat for cat in cats if not pd.isnull(cat)], ebay_categories))
     )
