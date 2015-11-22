9cad4c0256b1f3a77d7a3fc68b7bcbf4bd526ee2
Mateusz Flieger <mateuszflieger@gmail.com>
2015-11-22 19:20:18 +0100

    Adding ebay url

--- src/core/config.py
+++ src/core/config.py
@@ -37,7 +37,8 @@ EBAY_PRODUCTION_DOMAIN = 'ebay.com'
 
 # gifter API
 ITEMS_LIMIT = 6
-ITEM_DETAILS = ['title', 'sellingStatus', 'pictureURLSuperSize', 'primaryCategory']
+ITEM_DETAILS = ['title', 'sellingStatus', 'pictureURLSuperSize',
+                'primaryCategory', 'viewItemURL']
 ITEMS_ARGS_PARSER = {
     'min_price': Arg(int, default=None),
     'max_price': Arg(int, default=None),
