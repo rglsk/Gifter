43ce264ab2291e6ae374c4bb44bd5c40b527a0c8
Piotr Rogulski <rivinek@gmail.com>
2015-11-24 01:03:21 +0100

    Ngnix, uwsgi, dpkg configs

--- src/wsgi.py
+++ src/wsgi.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from gifter import create_app
 
+app = create_app(config_filename='core.config')
 
 if __name__ == '__main__':
-    app = create_app(config_filename='core.config')
     app.run(debug=True)
