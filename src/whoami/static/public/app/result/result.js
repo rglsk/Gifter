19a1f184519dfbb59bfda1a8fd36cfc2e8322c87
bckatarzyna <bckatarzyna@gmail.com>
2015-12-25 20:35:50 +0100

    whoami result page

--- src/whoami/static/public/app/result/result.js
+++ src/whoami/static/public/app/result/result.js
@@ -5,6 +5,12 @@ angular.module('whoami.result', [])
 
             this.twitterName = storageService.twitterName;
             this.person = storageService.person;
+            // this.twitterName = 'bckatarzyna';
+            // this.person = {
+            // 	'name': 'Khaleesi',
+            // 	'desc': 'You like dragons or another stupid staff chosen by our overfitted algorithm. But it\'s great, everybody wants to be her! Omg this app is so cool.',
+            // 	'imgUrl': '/assets/images/khaleesi.jpg'
+            // };
             this.category = storageService.category;
 
     		this.findNew = function() {
